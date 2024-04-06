57e2788834c5e6a659d1b76d1fc895540dc896cb
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-04-06 10:53:51 -0400

    create express backend and react frontend

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const onClick = async() =>{
+    try{
+      const resp = await fetch("http://localhost:9000/testAPI")
+      const text = await resp.text()
+      console.log("text", text)
+    } catch(e){
+
+    }
+
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={onClick}>test api</button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
