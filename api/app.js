a3b95d1741a43095c5d74aae1fb3e865c3a71ed5
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-04-08 09:51:18 -0400

    add Node version setting to package.json

--- api/app.js
+++ api/app.js
@@ -1,8 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cors = require("cors");
 
 var indexRouter = require('./routes/index');
@@ -12,10 +10,8 @@ var authorRouter = require("./routes/author");
 var app = express();
 
 app.use(cors())
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
